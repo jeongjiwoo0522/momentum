b966ec59350d883b5ab88604b128f1e5df090241
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-07-09 13:52:31 +0900

    modified1

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,17 @@
+const weather = document.querySelector('.js-weather');
+
 const API_KEY = "fcb510e330b6c9eb80bf1c95efc16188";
 const COORDS = 'coords';
 
 function getWeather(lat, lon) {
     fetch(
         `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-        ).then(function(json){
-            console.log(json);
+        ).then(function(response){
+            return response.json();
+        }).then(function(json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature}'@ ${place}`;
         });
 }
 
