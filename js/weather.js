a0b2a161c3bcdc38513927b2b06d8725cf2e20eb
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-10-30 00:01:02 +0900

    20201030 weather.js api

--- js/weather.js
+++ js/weather.js
@@ -7,6 +7,7 @@ function getWeather(lat, lon) {
     axios.get(
         `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
         ).then(function(response){
+            console.log(response);
             return response.json();
         }).then(function(json) {
             const temperature = json.main.temp;
